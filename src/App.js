ba5fa2de1cd0d421eba29fe18cb5dc78c3806dac
Ghada6336 <ghadafahmie6336@gmail.com>
2020-03-11 03:22:32 +0200

    task2 for javascript

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <img src="theindex.svg" className="logo" alt="the index logo"/>
           <section>
             <h4 className="menu-item active">
-              <button>AUTHORS</button>
+              <button>AUTHORS...</button>
             </h4>
           </section>
         </div>
